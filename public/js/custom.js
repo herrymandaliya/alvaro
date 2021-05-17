d33c361d77b58c1054ecc293c6c70764cebd97c9
Jalpesh Rajani <48354287+Jalpesh009@users.noreply.github.com>
2021-05-17 12:34:33 +0530

    first commit

---
+++ public/js/custom.js
@@ -0,0 +1,21 @@
+$(".select2_demo_2").select2({
+    theme: 'bootstrap4',
+});
+
+$('.custom-file-input').on('change', function() {
+    let fileName = $(this).val().split('\\').pop();
+    $(this).next('.custom-file-label').addClass("selected").html(fileName);
+});
+ 
+$(document).ready(function () {
+    $('.i-checks').iCheck({
+        checkboxClass: 'icheckbox_square-green',
+        radioClass: 'iradio_square-green',
+    });
+ 
+    $('.custom-file-input').on('change', function() {
+        let fileName = $(this).val().split('\\').pop();
+        $(this).next('.custom-file-label').addClass("selected").html(fileName);
+    });
+
+});
